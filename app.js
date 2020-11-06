ac471b0b8b646730c8eb3ea6f4ac91f09bd091ba
vinod473 <vinodkr00473@gmail.com>
2020-11-07 00:34:32 +0530

    delete and check button working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list'); 
 const todoInput = document.querySelector('.todo-input');
+
 todoButton.addEventListener('click',addTodo);
 
 function addTodo(e){
@@ -13,24 +14,35 @@ function addTodo(e){
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
-    //appending "li" to div
     todoDiv.appendChild(newTodo);
 
     //Creating Complete Button
     const completeButton = document.createElement('button');
     completeButton.classList.add('complete-btn');
-    completeButton.innerHTML = `<i class="far fa-check-circle"></i>`; 
-    //appending complete button to div
+    completeButton.innerHTML = `<i class="fas fa-check"></i>`;
     todoDiv.appendChild(completeButton);
 
     //creating Delete Button
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-btn');
     deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
-    //Appending delete button to Div
     todoDiv.appendChild(deleteButton);
 
-    //Finally appending the Div to list
     todoList.appendChild(todoDiv);
     todoInput.value = "";
 }
+
+todoList.addEventListener('click',function(e){
+    const item = e.target;
+    if(item.classList[0] === 'delete-btn'){
+        const todo = item.parentNode;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend',e =>{
+            todo.remove();
+        });
+    }
+    else if(item.classList[0]=== 'complete-btn'){
+        const todo = item.parentNode;
+        todo.classList.toggle('completed');
+    }
+})
